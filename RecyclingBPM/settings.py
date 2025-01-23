45c75ce97181b2330f18227eae6995a6eb63f37b
Mikhail <m.vinkler@gmail.com>
2025-01-23 23:09:45 +0100

    requirements.txt angelegt

--- RecyclingBPM/settings.py
+++ RecyclingBPM/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "warenwirtschaft" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
