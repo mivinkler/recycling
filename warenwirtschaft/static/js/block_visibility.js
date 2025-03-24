23277824388405d354f15057430c99efe6d31e7e
Mikhail Vinkler <m.vinkler@gmail.com>
2025-03-24 16:18:59 +0100

    supplier-list.html verbesserung

--- warenwirtschaft/static/js/block_visibility.js
+++ warenwirtschaft/static/js/block_visibility.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-<<<<<<< HEAD
     document.querySelectorAll(".navi-icon-js").forEach((iconDetail) => {
         const toggleButton = iconDetail.querySelector(".toggle-button");
         const hiddenBlock = iconDetail.querySelector(".hidden-block");
@@ -24,22 +23,5 @@ document.addEventListener("DOMContentLoaded", function () {
                 hiddenBlock.hidden = true;
             }
         });
-=======
-    const toggleButton = document.querySelector(".toggle-button");
-    const hiddenBlock = document.querySelector(".hidden-block");
-    const iconDetail = document.querySelector(".icon-detail");
-
-    // Открытие/закрытие блока при клике на кнопку
-    toggleButton.addEventListener("click", function (event) {
-        hiddenBlock.hidden = !hiddenBlock.hidden;
-        event.stopPropagation(); // Предотвращаем всплытие события, чтобы клики по кнопке не закрывали блок
-    });
-
-    // Закрытие блока, если клик был вне блока
-    document.addEventListener("click", function (event) {
-        if (!iconDetail.contains(event.target)) {
-            hiddenBlock.hidden = true; // Закрыть блок, если клик был не внутри элемента .icon-detail
-        }
->>>>>>> b42dd64a0d7b84e1ff64f2c73cf4ab2eab679c50
     });
 });
