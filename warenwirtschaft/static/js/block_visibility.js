d5da1ac62994a827b21c93b6769aa7671adf9542
Mikhail Vinkler <m.vinkler@gmail.com>
2025-03-24 11:34:10 +0100

    supplier_list.html update

--- warenwirtschaft/static/js/block_visibility.js
+++ warenwirtschaft/static/js/block_visibility.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+<<<<<<< HEAD
+    document.querySelectorAll(".navi-icon-js").forEach((iconDetail) => {
+        const toggleButton = iconDetail.querySelector(".toggle-button");
+        const hiddenBlock = iconDetail.querySelector(".hidden-block");
+
+        toggleButton.addEventListener("click", function (event) {
+            // Alle anderen Blöcke werden geschlossen, wenn ein neuer Block aufgemacht wird
+            document.querySelectorAll(".hidden-block").forEach((block) => {
+                if (block !== hiddenBlock) {
+                    block.hidden = true;
+                }
+            });
+
+            // Prüft, ob der Block ausgeblendet ist
+            hiddenBlock.hidden = !hiddenBlock.hidden;
+            // "stopPropagation()" - verhindert, dass der Block schließt, wenn darauf klicken
+            event.stopPropagation();
+        });
+
+        // Schließen eines Blocks, wenn außerhalb ihm geklickt wird
+        document.addEventListener("click", function (event) {
+            if (!iconDetail.contains(event.target)) {
+                hiddenBlock.hidden = true;
+            }
+        });
+=======
     const toggleButton = document.querySelector(".toggle-button");
     const hiddenBlock = document.querySelector(".hidden-block");
     const iconDetail = document.querySelector(".icon-detail");
@@ -14,5 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!iconDetail.contains(event.target)) {
             hiddenBlock.hidden = true; // Закрыть блок, если клик был не внутри элемента .icon-detail
         }
+>>>>>>> b42dd64a0d7b84e1ff64f2c73cf4ab2eab679c50
     });
 });
--- warenwirtschaft/views/supplier/supplier_list_view.py
+++ warenwirtschaft/views/supplier/supplier_list_view.py
@@ -11,26 +11,25 @@ class SupplierListView(ListView):
     context_object_name = "suppliers"
     paginate_by = 20
 
-    active_fields= [
-        "id", 
-        "avv_number", 
-        "name", 
-        "street", 
-        "postal_code", 
-        "city", 
-        "phone", 
-        "email", 
-        "note"
-        ]
-    
+    active_fields = [
+        ("id", "ID"),
+        ("avv_number", "AVV-Nummer"),
+        ("name", "Name"),
+        ("street", "Straße"),
+        ("postal_code", "PLZ"),
+        ("city", "Stadt"),
+        ("phone", "Telefon"),
+        ("email", "Email"),
+        ("note", "Bemerkung"),
+    ]
+
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        search_service = SearchService(self.request, self.active_fields)
-        sorting_service = SortingService(self.request, self.active_fields)
+        fields = [field[0] for field in self.active_fields]  # Wir nehmen nur die Schlüssel
 
-        queryset = search_service.apply_search(queryset)
-        queryset = sorting_service.apply_sorting(queryset)
+        queryset = SearchService(self.request, fields).apply_search(queryset)
+        queryset = SortingService(self.request, fields).apply_sorting(queryset)
 
         return queryset
 
@@ -42,4 +41,8 @@ class SupplierListView(ListView):
 
         context["page_obj"] = page_obj
         context["search_query"] = self.request.GET.get("search", "")
+        context["active_fields"] = self.active_fields
+        context["selected_menu"] = "supplier_list"
+
         return context
+
