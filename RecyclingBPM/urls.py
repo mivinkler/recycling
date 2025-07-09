126c69e619a53486c068a19053476460a7478019
Mikhail <m.vinkler@gmail.com>
2025-07-09 22:57:30 +0200

    header css

--- RecyclingBPM/urls.py
+++ RecyclingBPM/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from warenwirtschaft.views.delivery import DeliveryUnitsListView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('warenwirtschaft/', include("warenwirtschaft.urls")),
     path('api/', include('warenwirtschaft.api.urls')),
-    # path('', DeliveryUnitsListView.as_view(), name='home'),
+    path('', DeliveryUnitsListView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
